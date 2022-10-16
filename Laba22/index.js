06c9ed04da82d64adbb0dda61170131c6e3f1354
advxce <poznyak.dima2018@gmail.com>
2022-10-16 15:35:58 +0300

    Laba22

---
+++ Laba22/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $("a.thumbnail").click(function (e) {
+    e.preventDefault();
+
+    $("#image-modal .modal-body img").attr(
+      "src",
+      $(this).find("img").attr("src")
+    );
+    console.log($(this).find("img").attr("src"));
+    $("#image-modal").modal("show");
+  });
+
+  $("#image-modal .modal-body img").on("click", function () {
+    $("#image-modal").modal("hide");
+  });
+});
